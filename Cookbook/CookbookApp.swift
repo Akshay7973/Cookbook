47dbb44b6a306bf028bd059d6b7879a79c01bdbe
Akshay Gandal <akshaydgandal@gmail.com>
2026-02-21 11:12:59 +0530

    Initial Commit

---
+++ Cookbook/CookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookbookApp.swift
+//  Cookbook
+//
+//  Created by Akshay Gandal on 21/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CookbookTests/CookbookTests.swift
@@ -0,0 +1,17 @@
+//
+//  CookbookTests.swift
+//  CookbookTests
+//
+//  Created by Akshay Gandal on 21/02/26.
+//
+
+import Testing
+@testable import Cookbook
+
+struct CookbookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
